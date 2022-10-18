f6816fee60c821ab15719e10b691ec3b9ac9a151
rohith7869 <rbellapu@asu.edu>
2022-10-18 13:15:06 -0700

    FirstCommit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,30 @@
-package org.example;public class Main {
+package org.example;
+
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dateTime="1901-01-02T06:01:02.000Z";
+
+
+        try {
+            String formattedDate =
+                    new SimpleDateFormat("yyyy-MM-dd")
+                            .format(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").parse(dateTime));
+
+            //formattedDate="1901-01-02"
+
+            String formattedDate1 =
+                    new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+                            .format(new SimpleDateFormat("yyyy-MM-dd").parse(formattedDate));
+
+            //
+            System.out.println(formattedDate1);
+        } catch (ParseException e) {
+
+        }
     }
 }
